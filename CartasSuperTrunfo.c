b34e97098dd7550dce5410f180316618bbde0442
aguiarw7 <aguiarwallace2004@gmail.com>
2026-04-16 21:41:40 +0000

    Fiz o desafio nível aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,6 +17,10 @@ float pib1 ;
 
 int pontos_turisticos1 ;
 
+float densidade_populacional1 ;
+
+float pib_per_capita1 ;
+
 // Carta 2
 char estado2 ; // Escolha uma letra de A a H para representalo
 
@@ -28,10 +32,13 @@ int populacao2 ;
 
 float area2 ;
 
-float pib2 ; 
+float pib2 ;
 
 int pontos_turisticos2 ;
 
+float densidade_populacional2 ;
+
+float pib_per_capita2 ;
 
 // Entrada Carta 1
 printf("Digite o estado (A a H): ") ;
@@ -55,6 +62,10 @@ scanf("%f" , &pib1) ;
 printf("Digite os pontos Turisticos: ") ;
 scanf("%d" , &pontos_turisticos1) ;
 
+// Cálculos Carta 1
+densidade_populacional1 = populacao1 / area1 ;
+pib_per_capita1 = pib1 / populacao1 ;
+
 // Entrada Carta 2
 printf("Digite o estado (A a H): ") ;
 scanf(" %c" , &estado2) ;
@@ -77,6 +88,9 @@ scanf("%f" , &pib2) ;
 printf("Digite os pontos Turisticos: ") ;
 scanf("%d" , &pontos_turisticos2) ;
 
+// Cálculos Carta 2
+densidade_populacional2 = populacao2 / area2 ;
+pib_per_capita2 = pib2 / populacao2 ;
 
 // Exibição Carta 1
 printf("\n--- Carta 1 ---\n");
@@ -95,6 +109,10 @@ printf("PIB: %.2f\n" , pib1) ;
 
 printf("Pontos Turisticos: %d\n" , pontos_turisticos1) ;
 
+printf("Densidade Populacional: %.2f\n" , densidade_populacional1) ;
+
+printf("PIB per capita: %.2f\n" , pib_per_capita1) ;
+
 // Exibição Carta 2
 printf("\n--- Carta 2 ---\n");
 
@@ -112,6 +130,9 @@ printf("PIB: %.2f\n" , pib2) ;
 
 printf("Pontos Turisticos: %d\n" , pontos_turisticos2) ;
 
+printf("Densidade Populacional: %.2f\n" , densidade_populacional2) ;
+
+printf("PIB per capita: %.2f\n" , pib_per_capita2) ;
 
 return 0 ;
 
